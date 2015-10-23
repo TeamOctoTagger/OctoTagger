dd168e21b3fed12216c27e55607fc4cf8205dd24
Erik Leonard Ritschl <erikritschl@posteo.at>
2015-10-23 19:39:27 +0200

    Skeleton is element complete. Sizers need some more work.

--- edit_output_folder.py
+++ edit_output_folder.py
@@ -6,7 +6,7 @@ import wx
 class EditOutputFolder(wx.Frame):
     """Extending Frame"""
     def __init__(self, parent):
-        wx.Frame.__init__(self, parent, title = "Edit Output Folder", size = (600, 400))
+        wx.Frame.__init__(self, parent, title = "Edit Output Folder", size = (450, 350))
 
         self.InitUI()
         self.Show(True)
@@ -20,28 +20,49 @@ class EditOutputFolder(wx.Frame):
         # Define elements and add them to sizer
         # Labels
         txt_name = wx.StaticText(panel, label="Name: ")
-        sizer.Add(txt_name, pos=(0,0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, border=10)
+        sizer.Add(txt_name, pos=(0,0), flag=wx.LEFT|wx.TOP, border=5)
 
-#        txt_expression = wx.StaticText(panel, label="Expression: ")
-#        txt_directory = wx.StaticText(panel, label="Directory: ")
+        txt_expression = wx.StaticText(panel, label="Expression: ")
+        sizer.Add(txt_expression, pos=(1,0), flag=wx.LEFT|wx.TOP, border=5)
+
+        txt_directory = wx.StaticText(panel, label="Directory: ")
+        sizer.Add(txt_directory, pos=(2,0), flag=wx.LEFT|wx.TOP, border=5)
 
         # Text controls
         tc_name = wx.TextCtrl(panel)
-        sizer.Add(tc_name, pos=(0, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND)
-#        tc_expression = wx.TextCtrl(panel)
-#        tc_directory = wx.TextCtrl(panel)
+        sizer.Add(tc_name, pos=(0, 1), span=(1, 3), flag=wx.RIGHT|wx.TOP|wx.EXPAND, border=5)
 
-        # Define buttons
-#        btn_browse = wx.Button(panel, label="Browse...")
-#        btn_ok = wx.Button(panel, label="Ok")
-#        btn_cancel = wx.Button(panel, label="Cancel")
+        tc_expression = wx.TextCtrl(panel)
+        sizer.Add(tc_expression, pos=(1, 1), span=(1, 3), flag=wx.RIGHT|wx.TOP|wx.EXPAND, border=5)
 
-        # Add elements to sizer
-#        sizer.Add(txt_name, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, border=5)
-#        sizer.Add(tc_name, pos=(1, 0), span=(1, 5), flag=wx.EXPAND|wx.LEFT|wx.RIGHT, border=5)
+        tc_directory = wx.TextCtrl(panel)
+        sizer.Add(tc_directory, pos=(2, 1), span=(1, 3), flag=wx.RIGHT|wx.TOP|wx.EXPAND, border=5)
 
-#        sizer.Add(btn_ok, pos=(3, 3))
-#        sizer.Add(btn_cancel, pos=(3, 4), flag=wx.RIGHT|wx.BOTTOM, border=5)
+        # Buttons
+        btn_browse = wx.Button(panel, label="Browse...")
+        sizer.Add(btn_browse, pos=(2, 4), flag=wx.TOP|wx.RIGHT, border=5)
+
+        btn_ok = wx.Button(panel, label="Ok")
+        sizer.Add(btn_ok, pos=(5, 3), flag=wx.RIGHT|wx.BOTTOM, border=5)
+
+        btn_cancel = wx.Button(panel, label="Cancel")
+        sizer.Add(btn_cancel, pos=(5, 4), flag=wx.RIGHT|wx.BOTTOM, border=5)
+
+        # Advanced options
+        sbox_advanced_title = wx.StaticBox(panel, -1, "Advanced options")
+        sbox_advanced = wx.StaticBoxSizer(sbox_advanced_title, wx.VERTICAL)
+
+        txt_symlink = wx.StaticText(panel, label="Type of symbolic link used:")
+        sbox_advanced.Add(txt_symlink, flag=wx.LEFT|wx.TOP|wx.BOTTOM, border=5)
+
+        rb_softlinks = wx.RadioButton(panel, -1, "Softlinks", style = wx.RB_GROUP)
+        sbox_advanced.Add(rb_softlinks, flag=wx.LEFT|wx.BOTTOM, border=5)
+
+        rb_hardlinks = wx.RadioButton(panel, -1, "Hardlinks")
+        sbox_advanced.Add(rb_hardlinks, flag=wx.LEFT|wx.BOTTOM, border=5)
+
+        sizer.Add(sbox_advanced, pos=(3,0), span=(0, 5), flag=wx.LEFT|wx.TOP|wx.RIGHT|wx.EXPAND, border=5)
+
         sizer.AddGrowableCol(1)
         panel.SetSizer(sizer)
 
