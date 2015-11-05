8d2c3ff360e559bfb77ea6b7df380a4a7177f3b6
christophfuhrer <christoph.fuhrer@live.at>
2015-11-05 13:18:27 +0100

    Itemview Format Changes
    Inlines corrected & small code changes

--- itemview.py
+++ itemview.py
@@ -1,28 +1,30 @@
 import wx
 
+
 class ItemView(wx.Panel):
     def __init__(self, parent):
         super(ItemView, self).__init__(parent)
-	self.SetBackgroundColour(wx.LIGHT_GREY)
-	
-	self.sizer = wx.WrapSizer(wx.HORIZONTAL)
-	self.SetSizer(self.sizer)
-	self.SetAutoLayout(True)
+        self.SetBackgroundColour(wx.LIGHT_GREY)
+
+        self.sizer = wx.WrapSizer(wx.HORIZONTAL)
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(True)
 
     def SetItems(self, items):
         self.sizer.Clear()
-	for item in self.items:
-	    itemCtrl = Item(self, item['name'])
-	    self.sizer.Add(itemCtrl)
+        for item in items:
+            itemCtrl = Item(self, item['name'])
+            self.sizer.Add(itemCtrl)
+
 
 class Item(wx.Panel):
     def __init__(self, parent, name):
         super(Item, self).__init__(parent)
-	self.text = wx.StaticText(
-	    self,
-	    label=name,
-	    style=wx.ALIGN_CENTRE_HORIZONTAL
-	)
+        self.text = wx.StaticText(
+            self,
+            label=name,
+            style=wx.ALIGN_CENTRE_HORIZONTAL
+        )
 
 if __name__ == '__main__':
     app = wx.App(False)
