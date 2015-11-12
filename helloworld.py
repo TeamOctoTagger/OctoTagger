f0fa7e218f05834a3c993259d3f25c61bedafc61
Clemens Stadlbauer <ccstadlbauer@posteo.de>
2015-11-12 20:30:38 +0100

    Add more test Items to ItemView

--- helloworld.py
+++ helloworld.py
@@ -159,7 +159,16 @@ class MainWindow(wx.Frame):
         box.Add(panel2, 3, wx.EXPAND)
 
         panel2.SetItems([
-            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"}
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
+            {"name": "92996172-60c0-47e1-b447-d0bbeb9eab97"},
         ])
 
         self.SetSizer(box)
