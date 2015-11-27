2b5072d5a41cdc78b50f97f82fea458eb9b552dd
christophfuhrer <christoph.fuhrer@live.at>
2015-11-27 14:30:22 +0100

    Image Viewer Change

--- imageviewer.py
+++ imageviewer.py
@@ -1,3 +1,4 @@
+'''
 import os
 import wx
  
@@ -72,4 +73,45 @@ class PhotoCtrl(wx.App):
  
 if __name__ == '__main__':
     app = PhotoCtrl()
+    app.MainLoop()
+
+'''
+
+import wx
+
+class FooApp(wx.App):
+
+	# called when the 'change' button is pressed
+	def changeImage(self,event):
+		img = wx.Image("foo.jpg")
+		self.image = wx.StaticBitmap(self.panel, wx.ID_ANY, wx.BitmapFromImage(img))
+
+	def __init__(self):
+		# setup code for the window
+		wx.App.__init__(self)
+		self.frame = wx.Frame(None, title='Demo')
+		self.panel = wx.Panel(self.frame)
+
+		# load an image
+		img = wx.Image("landschaft.png")
+		self.image = wx.StaticBitmap(self.panel, wx.ID_ANY, wx.BitmapFromImage(img))
+
+		# create a Sizer to hold one (or more) button
+		self.buttons = wx.BoxSizer(wx.VERTICAL)
+		self.changeButton = wx.Button(self.panel, -1, "Change")
+		self.changeButton.Bind(wx.EVT_BUTTON,self.changeImage)
+		self.buttons.Add(self.changeButton)
+
+		self.mainSizer = wx.BoxSizer(wx.VERTICAL)
+		self.mainSizer.Add(self.image)
+		self.mainSizer.Add(self.buttons)
+
+		# more generic setupcode
+		self.panel.SetSizer(self.mainSizer)
+		self.mainSizer.Fit(self.frame)
+		self.panel.Layout()
+		self.frame.Show(True)
+
+if __name__ == '__main__':
+    app = FooApp()
     app.MainLoop()
