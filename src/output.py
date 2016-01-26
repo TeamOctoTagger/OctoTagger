d103499a7a615b490cfce8bf5d4bf38e7dd533dd
Clemens Stadlbauer <ccstadlbauer@posteo.de>
2016-01-26 20:28:12 +0100

    Implement change output name, location and tags

--- src/output.py
+++ src/output.py
@@ -2,6 +2,7 @@ import database
 import create_folders
 import expression
 import os
+import shutil
 
 
 def change(item, tag, create):
@@ -150,3 +151,178 @@ def remove(item):
     for folder in folders:
         path = os.path.join(folder[0], folder[1], filename)
         os.remove(path)
+
+
+def move(id, advanced, target):
+    connection = database.get_current_gallery("connection")
+    c = connection.cursor()
+
+    # get folder information
+    c.execute("SELECT location, name FROM ? WHERE pk_id=?", (
+        (advanced and "folder" or "gallery_folder"),
+        id,
+    ))
+    folder = c.fetchone()
+    if folder is None:
+        raise ValueError("No output folder with this id", id)
+    (source, name) = folder
+
+    # verify
+    if source == target:
+        raise ValueError("Source and target are the same")
+    if os.path.exists(os.path.join(target, name)):
+        raise IOError("Target path already exists")
+
+    # move
+    shutil.move(
+        os.path.join(source, name),
+        os.path.join(target, name),
+    )
+
+    # update db
+    c.execute(
+        (
+            "UPDATE ? "
+            "SET location=? "
+            "WHERE pk_id=?"
+        ),
+        (
+            (advanced and "folder" or "gallery_folder"),
+            target,
+            id,
+        )
+    )
+    connection.commit()
+
+
+def rename(id, advanced, new):
+    connection = database.get_current_gallery("connection")
+    c = connection.cursor()
+
+    # get folder information
+    c.execute("SELECT location, name FROM ? WHERE pk_id=?", (
+        (advanced and "folder" or "gallery_folder"),
+        id,
+    ))
+    folder = c.fetchone()
+    if folder is None:
+        raise ValueError("No output folder with this id", id)
+    (location, old) = folder
+
+    # verify
+    if old == new:
+        raise ValueError("New name must be different")
+    if os.path.exists(os.path.join(location, new)):
+        raise ValueError("New name already exists")
+
+    # move
+    shutil.move(
+        os.path.join(location, old),
+        os.path.join(location, new),
+    )
+
+    # update db
+    c.execute(
+        (
+            "UPDATE ? "
+            "SET name=? "
+            "WHERE pk_id=?"
+        ),
+        (
+            (advanced and "folder" or "gallery_folder"),
+            new,
+            id,
+        )
+    )
+    connection.commit()
+
+
+def change_gallery(id, tag, add):
+    connection = database.get_current_gallery("connection")
+    c = connection.cursor()
+
+    # get folder information
+    c.execute(
+        (
+            "SELECT location, name, use_softlink "
+            "FROM gallery_folder WHERE pk_id=?"
+        ),
+        (id,),
+    )
+    folder = c.fetchone()
+    if folder is None:
+        raise ValueError("No gallery folder with this id", id)
+
+    # check tag
+    c.execute(
+        (
+            "SELECT name FROM gallery_folder_has_tag "
+            "JOIN tag ON pk_fk_tag_id = pk_id "
+            "WHERE pk_fk_gallery_id=? AND pk_id=?"
+        ),
+        (id, tag),
+    )
+    tag_name = c.fetchone()
+    if tag_name is not None and add:
+        raise ValueError("Tag already added")
+    elif tag_name is None and not add:
+        raise ValueError("Tag already removed")
+    else:
+        tag_name = tag_name[0]
+
+    if add:
+        # get files
+        os.mkdir(os.path.join(folder[0], folder[1], tag_name))
+        c.execute(
+            (
+                "SELECT uuid, file_name "
+                "FROM file "
+                "JOIN file_has_tag ON pk_id = pk_fk_file_id "
+                "WHERE pk_fk_tag_id=?"
+            ),
+            (tag,),
+        )
+        files = c.fetchall()
+        gallery = database.get_current_gallery("directory")
+
+        # link files
+        if files is not None:
+            for file in files:
+                source = os.path.join(gallery, "files", file[0])
+                target = os.path.join(folder[0], folder[1], tag_name, file[1])
+                create_folders.symlink(source, target, folder[2])
+
+        # update db
+        c.execute(
+            (
+                "INSERT INTO gallery_folder_has_tag "
+                "VALUES (?, ?)"
+            ),
+            (id, tag),
+        )
+        connection.commit()
+    else:
+        # delete files
+        shutil.rmtree(os.path.join(folder[0], folder[1], tag_name))
+
+        # update db
+        c.execute(
+            (
+                "DELETE FROM gallery_folder "
+                "WHERE pk_fk_gallery_id=? AND "
+                "pk_fk_tag_id=?"
+            ),
+            (id, tag),
+        )
+        connection.commit()
+
+
+def change_link_type(id, advanced, type):
+    raise NotImplementedError()
+
+
+def change_expression(id, expression):
+    # TODO get folder
+    # TODO delete old files
+    # TODO link new files
+    raise NotImplementedError()
