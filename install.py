4fe8618bd9290b97e582247e5da5b54bca5bd8ff
Erik Ritschl <erikritschl@posteo.at>
2016-03-25 19:54:47 +0100

    Added autogeneration of run.sh file

--- install.py
+++ install.py
@@ -43,9 +43,24 @@ def write_desktop_file():
         desktop_file_path).st_mode | stat.S_IEXEC)
 
 def create_app_file():
-    run_script = os.path.join(os.getcwd(), "run.sh")
+    run_script_path = os.path.join(os.getcwd(), "run.sh")
+    run_script = open(
+        run_script_path,
+        "w"
+    )
+    run_script_content = (
+        '#!/bin/sh\n'
+        'export PYTHONPATH="/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages"\n'
+        'cd %s\n'
+        'python ./src/octotagger.py' %
+        (os.getcwd())
+    )
+    run_script.write(run_script_content)
+    os.chmod(run_script_path, os.stat(
+             run_script_path).st_mode | stat.S_IEXEC)
+
     app_file = "~/Desktop/OctoTagger.app"
-    command = ("osacompile -e 'do shell script \"%s\"' -x -o %s" % (run_script, app_file))
+    command = ("osacompile -e 'do shell script \"%s\"' -x -o %s" % (run_script_path, app_file))
     os.system(command)
 
     src = "./icons/logo.icns"
