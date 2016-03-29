4fb449480f4b1d003e10676ebbb49b2eb9241de2
christophfuhrer <christoph.fuhrer@live.at>
2016-03-29 21:49:01 +0200

    add correlation and bugfixes

--- src/suggestion.py
+++ src/suggestion.py
@@ -11,11 +11,13 @@ def get_suggestions(selected_files):
     recomms = []
     file_ids = []
     tag_quantities = []
+    tag_quant_temp = []
     no_append = False
     i = 0
     counter = 0
     contains_tag = False
     query_items = "SELECT pk_id FROM file"
+    final_ids = []
 
     cursor = database.get_current_gallery("connection").cursor()
     cursor.execute(query_items)
@@ -34,24 +36,30 @@ def get_suggestions(selected_files):
                 tag_quantities[counter][0] += 1
             counter += 1
 
-    if len(tag_ids) > 10:
-        while i < 10:
+    for tag_id in tag_ids:
+        tag_quant_temp.append([0, tag_id])
+
+    for tempFile in selected_files:
+        counter = 0
+        for tag_id in tag_ids:
+            if tagging.file_has_tag_id(tempFile, tag_id):
+                tag_quant_temp[counter][0] += 1
+            counter += 1
+
+    if len(tag_ids) > 7:
+        while i < 7:
             if not tag_quantities:
                 break
 
-            no_append = False
-            for selected_file in selected_files:
-                if tagging.file_has_tag_id(selected_file, max(tag_quantities)[1]):
-                    contains_tag = True
-                    no_append = True
-
-            if contains_tag is True:
-                tag_quantities.remove(max(tag_quantities))
-                contains_tag = False
-
-            if no_append is not True:
+            if max(tag_quant_temp)[0] < len(selected_files):
                 recomm_ids.append(max(tag_quantities)[1])
                 tag_quantities.remove(max(tag_quantities))
+                tag_quant_temp.remove(max(tag_quant_temp))
+                i += 1
+
+            else:
+                tag_quantities.remove(max(tag_quantities))
+                tag_quant_temp.remove(max(tag_quant_temp))
                 i += 1
         i = 0
     else:
@@ -59,24 +67,89 @@ def get_suggestions(selected_files):
             if not tag_quantities:
                 break
 
-            no_append = False
-            for selected_file in selected_files:
-                if tagging.file_has_tag_id(selected_file, max(tag_quantities)[1]):
-                    contains_tag = True
-                    no_append = True
-
-            if contains_tag is True:
-                tag_quantities.remove(max(tag_quantities))
-                contains_tag = False
-
-            if no_append is not True:
+            if max(tag_quant_temp)[0] < len(selected_files):
                 recomm_ids.append(max(tag_quantities)[1])
                 tag_quantities.remove(max(tag_quantities))
+                tag_quant_temp.remove(max(tag_quant_temp))
+                i += 1
+
+            else:
+                tag_quantities.remove(max(tag_quantities))
+                tag_quant_temp.remove(max(tag_quant_temp))
                 i += 1
         i = 0
 
-    for tag in recomm_ids:
+    tag_corr = []
+    corr_recomm_ids = []
+
+    for tag_id in tag_ids:
+        tag_corr.append([0, tag_id])
+
+    for tempFile in selected_files:
+        counter = 0
+        for tag_id in tag_ids:
+            if tagging.file_has_tag_id(tempFile, tag_id):
+                tag_corr[counter][0] += 1
+            counter += 1
+
+    if len(tag_ids) > 7:
+        while i < 7:
+            if not tag_corr:
+                break
+
+            if max(tag_corr)[0] < len(selected_files):
+                corr_recomm_ids.append(max(tag_corr)[1])
+                tag_corr.remove(max(tag_corr))
+                i += 1
+
+            else:
+                tag_corr.remove(max(tag_corr))
+                i += 1
+
+        i = 0
+    else:
+        while i < len(tag_ids):
+            if not tag_corr:
+                break
+
+            if max(tag_corr)[0] < len(selected_files):
+                corr_recomm_ids.append(max(tag_corr)[1])
+                tag_corr.remove(max(tag_corr))
+                i += 1
+
+            else:
+                tag_corr.remove(max(tag_corr))
+                i += 1
+
+        i = 0
+
+    if not selected_files:
+        corr_recomm_ids.append(1)
+        recomm_ids.append(1)
+
+    while True:
+        if not recomm_ids and not corr_recomm_ids:
+            break
+
+        if corr_recomm_ids[i] not in final_ids:
+            final_ids.append(corr_recomm_ids[i])
+
+        if recomm_ids[i] not in final_ids:
+            final_ids.append(recomm_ids[i])
+
+        if recomm_ids[i] is recomm_ids[len(recomm_ids)-1]:
+            break
+
+        if corr_recomm_ids[i] is corr_recomm_ids[len(corr_recomm_ids)-1]:
+            break
+
+        i += 1
+    i = 0
+
+    for tag in final_ids:
         recomms.append(tagging.tag_id_to_name(tag))
 
-    print recomms
+    print corr_recomm_ids
+    print recomm_ids
     return recomms
+
